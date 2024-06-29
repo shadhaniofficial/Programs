e4efb43992ba8b0054290e9ffcb4c06cfc337841
shadhani <shadhani@kali.Shadhani>
2024-06-29 18:58:11 +0530

    py

--- Python/Py/try.py
+++ Python/Py/try.py
@@ -236,8 +236,144 @@ def pattern14(n):
             print("*", end="")
         print()
 
+"""
+Enter the number of lines: 5
+
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1 
+
+"""
+def pattern15(n):
+    start = 1
+    for i in range(n+1):
+        if i % 2 == 0:
+            start = 0
+        else:
+            start = 1
+        for j in range(0,i):
+            print(start, end=" ")
+            start = 1 - start
+        print()
+        
+"""
+Enter the number of lines: 5
+
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+"""
+def pattern16(n):
+    for i in range(1,n+1):
+        #number
+        for j in range(1,i+1,1):
+            print(j,end="")
+            temp = j
+        #space
+        for k in range(2*n-temp-temp):
+            print(" ",end="")
+        #number
+        for l in range(i,0,-1):
+            print(l,end="")
+        print()
+
+"""
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+"""
+def pattern17(n):
+    for i in range(n,0,-1):
+        #number
+        for j in range(1,i+1,1):
+            print("*",end="")
+            temp = j
+        #space
+        for k in range(2*n-temp-temp):
+            print(" ",end="")
+        #number
+        for l in range(i,0,-1):
+            print("*",end="")
+        print()
+    for i in range(1,n+1):
+        #number
+        for j in range(1,i+1,1):
+            print("*",end="")
+            temp = j
+        #space
+        for k in range(2*n-temp-temp):
+            print(" ",end="")
+        #number
+        for l in range(i,0,-1):
+            print("*",end="")
+        print()
+        
+"""
+*****
+*   *
+*   *
+*   *
+*****
+"""
+def pattern18(n):
+    for i in range(n):
+        for j in range(n):
+            if i==0 or j==0 or i==n-1 or j==n-1:
+                print("*",end="")
+            else:
+                print(" ",end="")
+        print()
+
+"""
+4444
+4334
+4334
+4444
+"""
+def pattern19(n):
+    for i in range(n):
+        for j in range(n):
+            if i==0 or j==0 or i==n-1 or j==n-1:
+                print(f"{n}",end="")
+            else:
+                print(f"{n-1}",end="")
+        print()
+        
+"""
+Enter the number of lines: 3
+1 1 1 1 1 
+1 2 2 2 1 
+1 2 3 2 1 
+1 2 2 2 1 
+1 1 1 1 1 
+"""
+
+def pattern20(n):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            print(min(i,j),end=" ")
+        for j in range(n-1,0,-1):
+            print(min(i,j),end=" ")
+        print()
+    for i in range(n-1,0,-1):
+        for j in range(1,n+1):
+            print(min(i,j),end=" ")
+        for j in range(n-1,0,-1):
+            print(min(i,j),end=" ")
+        print()
 
 n = int(input("Enter the number of lines: "))
-pattern14(n)
+pattern20(n)
 
 
