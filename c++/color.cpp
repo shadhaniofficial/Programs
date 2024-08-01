b66ab3c6762f73693255ff17105b824815a14f75
sharvesh07 <156331034+sharvesh07@users.noreply.github.com>
2024-08-01 22:45:17 +0530

    Add files via upload

---
+++ c++/color.cpp
@@ -0,0 +1,84 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() 
+{ 
+    // Define color codes
+    //const string BOLD = "\033[1m";  
+    //const string RED_TEXT = "\033[92m";        // Red text
+    //const string LIGHT_YELLOW_BG = "\033[43m"; // Light yellow background
+    //const string RESET = "\033[0m";            // Reset to default colors
+
+    // Print the message with color
+    //cout << LIGHT_YELLOW_BG;
+    //cout << BOLD << RED_TEXT << "Geeks For Geeks!"; //<< RESET << endl; 
+    //cout << "hi";
+    
+    
+    
+//    cout << "\033[30m" << 30 << " BLACK" << endl;
+    cout << "\033[90m" << 90 << " DARK GREEN"<< endl;
+//    cout << "\033[31m" << 31 << " RED"<< endl;
+    cout << "\033[91m" << 91 << " RED"<< endl;
+//    cout << "\033[32m" << 32 << " GREEN" << endl;
+    cout << "\033[92m" << 92 << " GREEN" << endl;
+//    cout << "\033[33m" << 33 << " YELLOW"<< endl;
+    cout << "\033[93m" << 93 << " YELLOW" << endl;
+//    cout << "\033[34m" << 34 << " BLUE" << endl;
+    cout << "\033[94m" << 94 << " BLUE" << endl;
+//    cout << "\033[35m" << 35 << " VIOLET"<< endl;
+    cout << "\033[95m" << 95 << " VIOLET" << endl;
+//    cout << "\033[36m" << 36 << " LIGHT BLUE" << endl;
+    cout << "\033[96m" << 96 << " LIGHT BLUE" << endl;
+//    cout << "\033[37m" << 37 << " WHITE"<< endl;
+    cout << "\033[97m" << 97 << " WHITE" << endl;
+  
+    cout << "\033[0m" << 0 << " RESET" << endl;
+    
+    cout << "\033[1m" << 1 << " BOLD" << endl;
+    
+    cout << "\033[40m" << 40 << " BLACK BG"<< endl;
+    cout << "\033[100m" << 100 << " BRIGHT DARK GREEN BG"<< endl;
+    cout << "\033[41m" << 41 << " RED BG"<< endl;
+    cout << "\033[101m" << 101 << " BRIGHT RED BG"<< endl;
+    cout << "\033[42m" << 42 << " GREEN BG"<< endl;
+    cout << "\033[102m" << 102 << " BRIGHT GREEN BG" << endl;
+    cout << "\033[43m" << 43 << " YELLOW BG"<< endl;
+    cout << "\033[103m" << 103 << " BRIGHT YELLOW BG" << endl;
+    cout << "\033[44m" << 44 << " BLUE BG"<< endl;
+    cout << "\033[104m" << 104 << " BRIGHT BLUE BG" << endl;
+    cout << "\033[45m" << 45 << " VIOLET BG"<< endl;
+    cout << "\033[105m" << 105 << " BRIGHT VIOLET BG" << endl;
+    cout << "\033[46m" << 46 << " LIGHT BLUE BG"<< endl;
+    cout << "\033[106m" << 106 << " BRIGHT LIGHT BLUE BG" << endl;
+    cout << "\033[47m" << 47 << " WHITE BG"<< endl;
+    cout << "\033[107m" << 107 << " BRIGHT WHITE BG" << endl;
+
+    cout << "\033[0m" << 0 << " RESET" << endl;
+
+    cout << "\033[21m" << 21 << " UNDERLINE"<< endl;
+    
+    cout << "\033[30m" << 30 << " BLACK" << endl;
+    cout << "\033[31m" << 31 << " RED"<< endl;
+    cout << "\033[32m" << 32 << " GREEN" << endl;
+    cout << "\033[33m" << 33 << " YELLOW"<< endl;
+    cout << "\033[34m" << 34 << " BLUE" << endl;
+    cout << "\033[35m" << 35 << " VIOLET"<< endl;
+    cout << "\033[36m" << 36 << " LIGHT BLUE" << endl;
+    cout << "\033[37m" << 37 << " WHITE"<< endl;
+    
+    cout << "\033[0m" << 0 << " RESET" << endl;
+    
+//    cout << "\033[40m" << 40 << " BLACK BG"<< endl;
+//    cout << "\033[41m" << 41 << " RED BG"<< endl;
+//    cout << "\033[42m" << 42 << " GREEN BG"<< endl;
+//    cout << "\033[43m" << 43 << " YELLOW BG"<< endl;
+//    cout << "\033[44m" << 44 << " BLUE BG"<< endl;
+//    cout << "\033[45m" << 45 << " VIOLET BG"<< endl;
+//    cout << "\033[46m" << 46 << " LIGHT BLUE BG"<< endl;
+//    cout << "\033[47m" << 47 << " WHITE BG"<< endl;
+    
+    return 0; 
+}
+
